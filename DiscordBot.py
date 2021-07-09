1d3428b11d329a33927162b89b3921a3ac436bc4
sowens99 <seanowens@comcast.net>
2021-07-09 02:01:35 -0400

    Various improvements
    - Overall substantial spaghetti code cleanup
    - New method to retrieve YouTube links via a course-level YTLookup.txt file
    - Retrieve flap time by looking at the lowest lap amongst all 3 laps
    - Error handling on incorrect track provided

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,6 +1,7 @@
 # Discord-to-Github bot
 # Perform GitHub API calls based on parsed chat commands
 import os
+import re
 import discord
 from dotenv import load_dotenv
 from github import Github
@@ -52,106 +53,109 @@ track_folders = {
 
 category_folders = {
     'ng' : '/No Glitch',
-    'NG' : '/No Glitch',
     'nu' : '/No Ultra',
-    'NU' : '/No Ultra',
     'ur' : '/Unrestricted',
-    'UR' : '/Unrestricted'
 }
 
 lap_choices = {
     'flap' : 'Flap',
-    'Flap' : 'Flap',
     '3lap' : '3lap'
 }
 
 class Bot(discord.Client):
     async def on_ready(self):
         print(f'{self.user} is connected.')
+    
+    async def cmdHelp(self, cmd):
+        response = ''
+        if cmd in 'bkt':
+             response += f'```css\n!bkt track [category] [laps]\n```'
         
+        return response
+    
     async def on_message(self, msg):
         # Prevent recursion
         if msg.author == self.user:
             return
+            
         # Prevent bot calls from all channels except #mkw-git
         if msg.channel.id != CHANNEL_ID:
             return
         
-        '''
-        # Example to only allow TASLabz role members
-        role = discord.utils.find(lambda r: r.name == 'TASLabz', msg.channel.guild.roles)
-        if role in msg.author.roles:
-            print("Cool")
-        '''
-        
         # Check for bot command
         if len(msg.content) > 0 and msg.content[0] == CMD_PREFIX:
             await self.parseBotCmd(msg)
+        
         return
     
     async def parseBotCmd(self, msg):
         msgContent = msg.content[1:].split()
-        if msgContent[0] in {'bkt', 'BKT'}:
-            await self.bkt(msg, msgContent)
+        cmd = msgContent[0].lower()
+        
+        if cmd == 'bkt':
+            return await self.bkt(msg, msgContent)
     
     async def decodeTimes(self, binary, offset):
         minutes = int.from_bytes(binary[offset:offset+1], 'big') >> 1
         seconds = (int.from_bytes(binary[offset:offset+2], 'big') & 0x01fc) >> 2
-        ms = int.from_bytes(binary[offset+1:offset+3], 'big') & 0x03ff
+        ms      = int.from_bytes(binary[offset+1:offset+3], 'big') & 0x03ff
         
         # Assert format x:xx:xxx
         minutes = str(minutes)
-        seconds = str(seconds).zfill(2)
-        ms = str(ms).zfill(3)
+        seconds = str(seconds).zfill(2) #Pad with 0s
+        ms = str(ms).zfill(3) # Pad with 0s
         return minutes+':'+seconds+'.'+ms
     
-    async def get3lapTime(self, file):
-        if file.path[-4:] == '.txt':
+    async def getFileBinary(self, file):
+        try:
+            return file.decoded_content
+        except:
+            return None
+    
+    async def get3lapTime(self, binary, fileExtension):
+        if fileExtension == 'txt':
             # Ghost does not exist in repo
-            # Retrieve time from file`
-            binary = file.decoded_content.decode().split('\n')
-            return binary[0]
-            
-        binary = file.decoded_content
+            # Retrieve time from file
+            return binary.decode().split('\n')[0]
         
         return await self.decodeTimes(binary, 0x4)
     
-    async def getlapTimes(self, file):
-        if file.path[-4:] == '.txt':
+    async def getlapTimes(self, binary, fileExtension):
+        if fileExtension == 'txt':
             # Ghost does not exist in repo
             # Retrieve lap times from file`
-            binary = file.decoded_content.decode().split('\n')
+            binary = binary.decode().split('\n')
             return [str(binary[1]), str(binary[2]), str(binary[3])]
         
-        binary = file.decoded_content
-        laps = []
-        for lap in range (0, 3):
-            offset = 0x11  + (lap * 3)
-            lapTime = await self.decodeTimes(binary, offset)
-            laps.append(lapTime)
-        return laps
+        # Decode lap time in binary at 0x11 + (x*3)
+        # where x is the lap #
+        # and 3 represents the # of bytes for each lap's time structure
+        return [await self.decodeTimes(binary, 0x11 + (x * 3)) for x in range(0, 3)]
         
-    async def getPlaceholderYTLink(self, file):
-        binary = file.decoded_content.decode().split('\n')
-        return binary[-2]
+    async def getYTLink(self, file, category, laps):
+        try:
+            binary = file.decoded_content.decode()
+        except:
+            return '' # Empty string prevents link creation
+            
+        for line in binary.split('\n'):
+            if all(x in line for x in [category, laps]):
+                    return line.split()[-1]
+        return '' # Empty string prevents link creation
     
     async def getCategory(self, folder, folderContents, msgContent):
         if folderContents[0].type == 'dir':
             # We see folders under the course, so we need categories
             try:
-                folder += category_folders[msgContent[2]]
+                # Append this subfolder to the folder path
+                folder += category_folders[msgContent[2].lower()]
+                
+                # Strip out category (to indistinguish this from cmd with no category specified)
+                msgContent = msgContent[0:2] + msgContent[3:]
             except:
                 # Invalid or missing category - Link all ghosts
-                return (folder, msgContent)
-        
-            # Check for empty category folder
-            folderContents = repo.get_contents(folder)
-            if folderContents is None:
-                return (None, None)
-            
-            # Strip out category
-            msgContent = msgContent[0:2] + msgContent[3:]
-                
+                pass
+      
         return (folder, msgContent)
     
     async def getFiles(self, msg, laps, folder):
@@ -159,64 +163,115 @@ class Bot(discord.Client):
         folderContents = repo.get_contents(folder)
         for content in folderContents:
             if content.type == 'dir':
+                # Recurse down to this folder (category)
                 temp = await self.getFiles(msg, laps, content.path)
                 if temp is not None:
                     files += temp
-            elif laps in content.name:
+            elif laps in content.name and content.name != 'YTLookup.txt':
                 files.append(content)
         return files
+        
+    async def getFastestLapIndex(self, laptimes):
+        # Parse laptime string as seconds for comparison
+        lapSeconds = []
+        for i in range(0, len(laptimes)):
+            lap = laptimes[i]
+            laptime = 0
+            if lap == 'Unknown' or lap == 'N/A':
+                continue
+                
+            # Separate lap times by minutes,seconds,ms
+            lapComponents = re.split(':|\.', lap)
+            
+            if len(lapComponents) == 3: # Minutes
+                laptime += (60 * int(lapComponents[0]))
+                lapComponents.pop(0)
+            if len(lapComponents) == 2: # Seconds
+                laptime += (int(lapComponents[0]))
+                lapComponents.pop(0)
+            if len(lapComponents) == 1: # Milliseconds
+                laptime += (.001 * int(lapComponents[0]))
+                
+            # Store [time, index]
+            lapSeconds.append([laptime, i])
+        
+        # Sort laps based off of time
+        lapSeconds.sort(key = lambda x: x[0])
+        return lapSeconds[0][1]
     
-    async def parseBKTLinks(self, msg, files):
+    async def bktEmbed(self, msg, files):
         track = files[0].path.split('/')[0][4:]
         
-        # Embed post
-        embed = discord.Embed(title=track, description="BKT", color=0x008800)
+        # Embed obj
+        embed = discord.Embed(title=track, description="BKTs", color=0x89DA72)
         
-        #response = f"**{track}**\n"
         last_cat = ''
         value = ''
-        for file in files:
-            fileDirs = file.path.split('/')
-            if len(fileDirs) == 2:
-                # There are no categories on this course
-                category = 'Unrestricted'
-            else:
-                category = fileDirs[-2]
+        
+        for bkt in files:
+            fileDirs = bkt.path.split('/')
             
-            # If we have cached one ghost which is a different category from the current ghost,
-            # Add embed field for the cached ghost
+            # Check if no categories on this course
+            category = 'Unrestricted' if len(fileDirs) == 2 else fileDirs[-2]
+            
+            # Check if the last ghost we checked was in a different category
+            # If so, we want to print them separately in the embed post
             if last_cat not in {category, ''}:
                 embed.add_field(name=last_cat, value=value, inline=False)
                 value = ''
             
+            binary = await self.getFileBinary(bkt)
+            fileExtension = bkt.path.split('.')[-1]
+            time = await self.get3lapTime(binary, fileExtension)
             laps = fileDirs[-1][:4]
-            time = await self.get3lapTime(file)
-            laptimes = await self.getlapTimes(file)
-            fileURL = repo.html_url + "/blob/main/" + file.path.replace(' ', '%20') + "?raw=true"
+            laptimes = await self.getlapTimes(binary, fileExtension)
             
-            linkText = 'DL'
+            rkgLink = repo.html_url + "/blob/main/" + bkt.path.replace(' ', '%20') + "?raw=true"
+            linkText = '[DL]'
+            ytLookupFile = repo.get_contents(fileDirs[0])[-1]
+            ytLink = await self.getYTLink(ytLookupFile, category, laps)
             
-            if file.path[-4:] == '.txt':
+            if bkt.path[-4:] == '.txt':
                 # Remove embed link since not a valid ghost file
-                linkText = 'No ghost on repo'
-                fileURL = await self.getPlaceholderYTLink(file)
+                rkgLink = ''
+                linkText = ''
             
-            value+=f"[{laps}]({fileURL}) - {time} ({laptimes[0]}, {laptimes[1]}, {laptimes[2]})\n"
+            value += f"[{laps}]({ytLink}) - "
             
-            # In order to handle groupings of 3lap+flap for one category field,
-            # do one of the following
-            if file == files[-1]: # if last ghost, force field add
-                embed.add_field(name=category, value=value, inline=False)
-            elif last_cat == category: # We looked at 3lap, now looking at flap, done w/ category so add field
-                embed.add_field(name=category, value=value, inline=False)
-                value = ''
-                last_cat = ''
-            else: # there may be another ghost for this category so wait to check the next ghost
-                last_cat = category
-                
+            if laps == '3lap':
+                value+=f"{time} ({laptimes[0]}, {laptimes[1]}, {laptimes[2]})"
+            elif laps == 'Flap':
+                fastLapIndex = await self.getFastestLapIndex(laptimes)
+                value+=f"{laptimes[fastLapIndex]} (Final Time: {time})"
+            
+            value += f" [{linkText}]({rkgLink})\n"
+
+            last_cat = category
+        
+        # Since we're done iterating the list of ghost files, add the last ghost
+        embed.add_field(name=category, value=value, inline=False)
+        
         if embed.fields == discord.Embed.Empty:
             return
+        await msg.channel.send(embed=embed)
+    
+    async def printTracks(self, msg):
+        response = '```LC     MC     DC     DDR\n' \
+                      'MMM    CM     KC     MH\n' \
+                      'MG     DKS    MT     BC\n' \
+                      'TF     WGM    GV     RR\n\n' \
+                      'rPB    rSL    rDH    rMC3\n' \
+                      'rYF    rSGB   rBC3   rPG\n' \
+                      'rGV2   rDS    rDKJP  rDKM\n' \
+                      'rMR    rWS    rMC    rBC```'
+        return response
+    
+    async def embedError(self, msg, error, cmd, details):
+        cmdHelp = await self.cmdHelp(cmd)
+        if details == 'track':
+            cmdHelp += "\nUse one of the following:\n" + await self.printTracks(msg)
             
+        embed = discord.Embed(title=error, description=cmdHelp, color=0xC13353)
         await msg.channel.send(embed=embed)
     
     async def bkt(self, msg, msgContent):
@@ -224,29 +279,27 @@ class Bot(discord.Client):
         try:
             track_folder = track_folders[msgContent[1].lower()]
         except:
-            return
+            if len(msgContent) == 1:
+                error = "No track provided."
+                await self.embedError(msg, error, 'bkt', 'track')
+                return
         
-        # Check for empty course folder
         folderContents = repo.get_contents(track_folder)
-        if folderContents is None:
-            return
         
+        # Get category (if provided) and remove it from the msgContent to handle both cases the same way
         (cat_folder, msgContent) = await self.getCategory(track_folder, folderContents, msgContent)
         
         # Handle the case when no 3lap/flap is provided
         try:
-            laps = lap_choices[msgContent[2]]
+            laps = lap_choices[msgContent[2].lower()]
         except:
             laps = ''
         
-        # Check if we should get all ghost files
+        # Get all relevant ghost files
         files = await self.getFiles(msg, laps, cat_folder)
-        
-        if files == [None]:
-            return
             
         # Parse the URLs
-        await self.parseBKTLinks(msg, files)
+        await self.bktEmbed(msg, files)
 
 def getRepoObj():
     for repo in g.get_user().get_repos():
